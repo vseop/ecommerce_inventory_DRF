59728a22ce6efbcc247f29a37751c5ce410d3de6
vseop <ivpython@mail.ru>
2022-07-11 18:45:24 +0300

    add db_brand_fixture, create Brand,ProductType,ProductInventory factories

--- ecommerce/demo/management/commands/load-fixtures.py
+++ ecommerce/demo/management/commands/load-fixtures.py
@@ -9,3 +9,4 @@ class Command(BaseCommand):
         call_command("loaddata", "db_admin_fixture.json")
         call_command("loaddata", "db_category_fixture.json")
         call_command("loaddata", "db_product_fixture.json")
+        call_command("loaddata", "db_product_inventory_fixture.json")
--- ecommerce/tests/factories.py
+++ ecommerce/tests/factories.py
@@ -39,5 +39,38 @@ class ProductFactory(factory.django.DjangoModelFactory):
                 self.category.add(cat)
 
 
+class ProductTypeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.ProductType
+
+    name = factory.Sequence(lambda n: "type_%d" % n)
+
+
+class BrandFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Brand
+
+    name = factory.Sequence(lambda n: "brand_%d" % n)
+
+
+class ProductInventoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.ProductInventory
+
+    sku = factory.Sequence(lambda n: "sku_%d" % n)
+    upc = factory.Sequence(lambda n: "upc_%d" % n)
+    product_type = factory.SubFactory(ProductTypeFactory)
+    product = factory.SubFactory(ProductFactory)
+    brand = factory.SubFactory(BrandFactory)
+    is_active = 1
+    retail_price = 97
+    store_price = 92
+    sale_price = 46
+    weight = 987
+
+
 register(CategoryFactory)
 register(ProductFactory)
+register(ProductTypeFactory)
+register(BrandFactory)
+register(ProductInventoryFactory)
