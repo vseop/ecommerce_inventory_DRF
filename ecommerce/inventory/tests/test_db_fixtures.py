be60469a086872e65686a266eaeb0e9970a401cd
vseop <ivpython@mail.ru>
2022-07-11 20:04:37 +0300

    test media

--- ecommerce/inventory/tests/test_db_fixtures.py
+++ ecommerce/inventory/tests/test_db_fixtures.py
@@ -210,8 +210,8 @@ def test_inventory_db_producttype_uniqueness_integrity(
     with pytest.raises(IntegrityError):
         product_type_factory.create(name="not_unique")
 
-def test_inventory_db_brand_insert_data(db, brand_factory):
 
+def test_inventory_db_brand_insert_data(db, brand_factory):
     new_brand = brand_factory.create(name="demo_brand")
     assert new_brand.name == "demo_brand"
 
@@ -220,3 +220,57 @@ def test_inventory_db_brand_uniqueness_integrity(db, brand_factory):
     brand_factory.create(name="not_unique")
     with pytest.raises(IntegrityError):
         brand_factory.create(name="not_unique")
+
+
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "id, product_inventory, image, alt_text, is_feature, created_at, updated_at",
+    [
+        (
+                1,
+                1,
+                "images/default.png",
+                "a default image solid color",
+                1,
+                "2021-09-04 22:14:18",
+                "2021-09-04 22:14:18",
+        ),
+        (
+                8616,
+                8616,
+                "images/default.png",
+                "a default image solid color",
+                1,
+                "2021-09-04 22:14:18",
+                "2021-09-04 22:14:18",
+        ),
+    ],
+)
+def test_inventory_db_media_dataset(
+        db,
+        db_fixture_setup,
+        id,
+        product_inventory,
+        image,
+        alt_text,
+        is_feature,
+        created_at,
+        updated_at,
+):
+    result = models.Media.objects.get(id=id)
+    result_created_at = result.created_at.strftime("%Y-%m-%d %H:%M:%S")
+    result_updated_at = result.updated_at.strftime("%Y-%m-%d %H:%M:%S")
+    assert result.product_inventory.id == product_inventory
+    assert result.image == image
+    assert result.alt_text == alt_text
+    assert result.is_feature == is_feature
+    assert result_created_at == created_at
+    assert result_updated_at == updated_at
+
+
+def test_inventory_db_media_insert_data(db, media_factory):
+    new_media = media_factory.create(product_inventory__sku="123456789")
+    assert new_media.product_inventory.sku == "123456789"
+    assert new_media.image == "images/default.png"
+    assert new_media.alt_text == "a default image solid color"
+    assert new_media.is_feature == 1
