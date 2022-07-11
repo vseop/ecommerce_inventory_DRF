122ba926446b7e0e5c0ee2d6af097d74f0bf77e1
vseop <ivpython@mail.ru>
2022-07-11 17:45:48 +0300

    add model ProductInventory

--- ecommerce/inventory/models.py
+++ ecommerce/inventory/models.py
@@ -113,3 +113,107 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProductInventory(models.Model):
+    """
+    Product inventory table
+    """
+
+    sku = models.CharField(
+        max_length=20,
+        unique=True,
+        null=False,
+        blank=False,
+        verbose_name=_("stock keeping unit"),
+        help_text=_("format: required, unique, max-20"),
+    )
+    upc = models.CharField(
+        max_length=12,
+        unique=True,
+        null=False,
+        blank=False,
+        verbose_name=_("universal product code"),
+        help_text=_("format: required, unique, max-12"),
+    )
+    product_type = models.ForeignKey(
+        ProductType, related_name="product_type", on_delete=models.PROTECT
+    )
+    product = models.ForeignKey(
+        Product, related_name="product", on_delete=models.PROTECT
+    )
+    brand = models.ForeignKey(
+        Brand, related_name="brand", on_delete=models.PROTECT
+    )
+    attribute_values = models.ManyToManyField(
+        ProductAttributeValue,
+        related_name="product_attribute_values",
+        through="ProductAttributeValues",
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name=_("product visibility"),
+        help_text=_("format: true=product visible"),
+    )
+    retail_price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        unique=False,
+        null=False,
+        blank=False,
+        verbose_name=_("recommended retail price"),
+        help_text=_("format: maximum price 999.99"),
+        error_messages={
+            "name": {
+                "max_length": _("the price must be between 0 and 999.99."),
+            },
+        },
+    )
+    store_price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        unique=False,
+        null=False,
+        blank=False,
+        verbose_name=_("regular store price"),
+        help_text=_("format: maximum price 999.99"),
+        error_messages={
+            "name": {
+                "max_length": _("the price must be between 0 and 999.99."),
+            },
+        },
+    )
+    sale_price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        unique=False,
+        null=False,
+        blank=False,
+        verbose_name=_("sale price"),
+        help_text=_("format: maximum price 999.99"),
+        error_messages={
+            "name": {
+                "max_length": _("the price must be between 0 and 999.99."),
+            },
+        },
+    )
+    weight = models.FloatField(
+        unique=False,
+        null=False,
+        blank=False,
+        verbose_name=_("product weight"),
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        editable=False,
+        verbose_name=_("date sub-product created"),
+        help_text=_("format: Y-m-d H:M:S"),
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_("date sub-product updated"),
+        help_text=_("format: Y-m-d H:M:S"),
+    )
+
+    def __str__(self):
+        return self.product.name
--- ecommerce/inventory/tests/test_db_fixtures.py
+++ ecommerce/inventory/tests/test_db_fixtures.py
@@ -106,6 +106,93 @@ def test_inventory_db_product_insert_data(
     assert result_product_category == 5
 
 
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "id, sku, upc, product_type, product, brand, is_active, retail_price, store_price, sale_price, weight, created_at, updated_at",
+    [
+        (
+                1,
+                "7633969397",
+                "934093051374",
+                1,
+                1,
+                1,
+                1,
+                97.00,
+                92.00,
+                46.00,
+                987,
+                "2021-09-04 22:14:18",
+                "2021-09-04 22:14:18",
+        ),
+        (
+                8616,
+                "3880741573",
+                "844935525855",
+                1,
+                8616,
+                1253,
+                1,
+                89.00,
+                84.00,
+                42.00,
+                929,
+                "2021-09-04 22:14:18",
+                "2021-09-04 22:14:18",
+        ),
+    ],
+)
+def test_inventory_db_product_inventory_dataset(
+        db,
+        db_fixture_setup,
+        id,
+        sku,
+        upc,
+        product_type,
+        product,
+        brand,
+        is_active,
+        retail_price,
+        store_price,
+        sale_price,
+        weight,
+        created_at,
+        updated_at,
+):
+    result = models.ProductInventory.objects.get(id=id)
+    result_created_at = result.created_at.strftime("%Y-%m-%d %H:%M:%S")
+    result_updated_at = result.updated_at.strftime("%Y-%m-%d %H:%M:%S")
+    assert result.sku == sku
+    assert result.upc == upc
+    assert result.product_type.id == product_type
+    assert result.product.id == product
+    assert result.brand.id == brand
+    assert result.is_active == is_active
+    assert result.retail_price == retail_price
+    assert result.store_price == store_price
+    assert result.sale_price == sale_price
+    assert result.weight == weight
+    assert result_created_at == created_at
+    assert result_updated_at == updated_at
 
 
-
+def test_inventory_db_product_inventory_insert_data(
+        db, product_inventory_factory
+):
+    new_product = product_inventory_factory.create(
+        sku="123456789",
+        upc="123456789",
+        product_type__name="new_name",
+        product__web_id="123456789",
+        brand__name="new_name",
+    )
+    assert new_product.sku == "123456789"
+    assert new_product.upc == "123456789"
+    assert new_product.product_type.name == "new_name"
+    assert new_product.product.web_id == "123456789"
+    assert new_product.brand.name == "new_name"
+    assert new_product.is_active == 1
+    assert new_product.retail_price == 97.00
+    assert new_product.store_price == 92.00
+    assert new_product.sale_price == 46.00
+    assert new_product.weight == 987
