f19548f4cbd7eedafdecd71e7e68b12dd331dc3e
vseop <ivpython@mail.ru>
2022-07-12 17:58:43 +0300

    create media (model and fixture)

--- ecommerce/inventory/models.py
+++ ecommerce/inventory/models.py
@@ -251,3 +251,51 @@ class ProductInventory(models.Model):
 
     def __str__(self):
         return self.product.name
+
+class Media(models.Model):
+    """
+    The product image table.
+    """
+
+    product_inventory = models.ForeignKey(
+        ProductInventory,
+        on_delete=models.PROTECT,
+        related_name="media_product_inventory",
+    )
+    image = models.ImageField(
+        unique=False,
+        null=False,
+        blank=False,
+        verbose_name=_("product image"),
+        upload_to="images/",
+        default="images/default.png",
+        help_text=_("format: required, default-default.png"),
+    )
+    alt_text = models.CharField(
+        max_length=255,
+        unique=False,
+        null=False,
+        blank=False,
+        verbose_name=_("alternative text"),
+        help_text=_("format: required, max-255"),
+    )
+    is_feature = models.BooleanField(
+        default=False,
+        verbose_name=_("product default image"),
+        help_text=_("format: default=false, true=default image"),
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        editable=False,
+        verbose_name=_("product visibility"),
+        help_text=_("format: Y-m-d H:M:S"),
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_("date sub-product created"),
+        help_text=_("format: Y-m-d H:M:S"),
+    )
+
+    class Meta:
+        verbose_name = _("product image")
+        verbose_name_plural = _("product images")
