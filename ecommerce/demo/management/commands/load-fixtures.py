1366bb16c0b6b507c66e920f1f5a8ff3ce44a446
vseop <ivpython@mail.ru>
2022-07-21 19:34:08 +0300

    add commands

--- ecommerce/demo/management/commands/load-fixtures.py
+++ ecommerce/demo/management/commands/load-fixtures.py
@@ -9,6 +9,7 @@ class Command(BaseCommand):
         call_command("loaddata", "db_admin_fixture.json")
         call_command("loaddata", "db_category_fixture.json")
         call_command("loaddata", "db_product_fixture.json")
+        call_command("loaddata", "db_category_product_fixture.json")
         call_command("loaddata", "db_type_fixture.json")
         call_command("loaddata", "db_brand_fixture.json")
         call_command("loaddata", "db_product_inventory_fixture.json")
@@ -17,5 +18,4 @@ class Command(BaseCommand):
         call_command("loaddata", "db_product_attribute_fixture.json")
         call_command("loaddata", "db_product_attribute_value_fixture.json")
         call_command("loaddata", "db_product_attribute_values_fixture.json")
-        call_command("loaddata", "db_category_product_fixture.json")
-
+        call_command("loaddata", "db_product_type_attribute_fixture.json")
