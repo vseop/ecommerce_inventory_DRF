e3c79745a281d1d254345b4f2d079eb34c1b9288
vseop <ivpython@mail.ru>
2022-07-21 18:41:38 +0300

    demo updated product_detail

--- ecommerce/demo/views.py
+++ ecommerce/demo/views.py
@@ -25,8 +25,8 @@ def product_by_category(request, category):
 
     return render(request, "product_by_category.html", {"data": y})
 
-def product_detail(request, slug):
 
+def product_detail(request, slug):
     # x = models.Product.objects.filter(slug=slug)
 
     # x = models.ProductInventory.objects.filter(product__slug=slug).values("id", "product__name", "store_price", "is_default", "attribute_values__attribute_value")
@@ -37,3 +37,34 @@ def product_detail(request, slug):
     # from django.db.models import Count
     # filter_arguments = [5, "red"]
     # x = models.ProductInventory.objects.filter(product__slug=slug).filter(attribute_values__attribute_value__in=filter_arguments).annotate(num_tags=Count('attribute_values')).filter(num_tags=len(filter_arguments))
+
+    # Dynamic Filter
+    filter_arguments = []
+
+    if request.GET:
+        for value in request.GET.values():
+            filter_arguments.append(value)
+
+        from django.db.models import Count
+        from django.contrib.postgres.aggregates import ArrayAgg
+
+        x = models.ProductInventory.objects.filter(product__slug=slug).filter(
+            attribute_values__attribute_value__in=filter_arguments).annotate(num_tags=Count('attribute_values')).filter(
+            num_tags=len(filter_arguments)).values(
+            "id", "sku", "product__name", "store_price", "product_inventory__units").annotate(
+            field_a=ArrayAgg("attribute_values__attribute_value")).get()
+    else:
+
+        from django.contrib.postgres.aggregates import ArrayAgg
+
+        x = models.ProductInventory.objects.filter(product__slug=slug).filter(is_default=True).values(
+            "id", "sku", "product__name", "store_price", "product_inventory__units").annotate(
+            field_a=ArrayAgg("attribute_values__attribute_value")).get()
+
+    y = models.ProductInventory.objects.filter(product__slug=slug).distinct().values(
+        "attribute_values__product_attribute__name", "attribute_values__attribute_value")
+
+    z = models.ProductTypeAttribute.objects.filter(product_type__product_type__product__slug=slug).distinct().values(
+        "product_attribute__name")
+
+    return render(request, "product_detail.html", {"x": x, "filter": y, "z": z})
