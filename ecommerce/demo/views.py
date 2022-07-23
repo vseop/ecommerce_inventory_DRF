b521a8f456dd660a4cb1a0f16ab98703511c07da
vseop <ivpython@mail.ru>
2022-07-23 21:00:03 +0300

    completed demo

--- ecommerce/demo/views.py
+++ ecommerce/demo/views.py
@@ -1,6 +1,8 @@
 # from django.core import serializers
 from django.shortcuts import render
 from ecommerce.inventory import models
+from django.db.models import Count
+from django.contrib.postgres.aggregates import ArrayAgg
 
 
 def home(request):
@@ -31,37 +33,26 @@ def product_detail(request, slug):
     # x = models.ProductInventory.objects.filter(product__slug=slug).values(
     # "id", "product__name", "store_price", "is_default", "attribute_values__attribute_value")
 
-    # Using chained filters approach
     # x = models.ProductInventory.objects.filter(product__slug=slug).filter(
     # attribute_values__attribute_value="red").filter(attribute_values__attribute_value=5).select_related('product')
 
-    # from django.db.models import Count
     # filter_arguments = [5, "red"]
     # x = models.ProductInventory.objects.filter(product__slug=slug).filter(
     # attribute_values__attribute_value__in=filter_arguments).annotate(
     # num_tags=Count('attribute_values')).filter(num_tags=len(filter_arguments))
 
-    # Dynamic Filter
     filter_arguments = []
 
     if request.GET:
         for value in request.GET.values():
             filter_arguments.append(value)
 
-        from django.db.models import Count
-        from django.contrib.postgres.aggregates import ArrayAgg
-
         x = models.ProductInventory.objects.filter(product__slug=slug).filter(
             attribute_values__attribute_value__in=filter_arguments).annotate(num_tags=Count('attribute_values')).filter(
             num_tags=len(filter_arguments)).values(
             "id", "sku", "product__name", "store_price", "product_inventory__units").annotate(
             field_a=ArrayAgg("attribute_values__attribute_value")).get()
-
-
     else:
-
-        from django.contrib.postgres.aggregates import ArrayAgg
-
         x = models.ProductInventory.objects.filter(product__slug=slug).filter(is_default=True).values(
             "id", "sku", "product__name", "store_price", "product_inventory__units").annotate(
             field_a=ArrayAgg("attribute_values__attribute_value")).get()
