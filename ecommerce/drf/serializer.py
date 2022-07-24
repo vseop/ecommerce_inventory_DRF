f2deb53c473609cde80f8b258e35129aeec08423
vseop <ivpython@mail.ru>
2022-07-24 19:41:03 +0300

    add drf.serializers

---
+++ ecommerce/drf/serializer.py
@@ -0,0 +1,27 @@
+from django.db.models.fields.related import ManyToManyField
+from ecommerce.inventory.models import (
+    Brand,
+    Category,
+    Media,
+    Product,
+    ProductAttributeValue,
+    ProductInventory,
+    ProductType,
+)
+from rest_framework import serializers
+
+
+
+
+class AllProductSerializer(serializers.HyperlinkedModelSerializer):
+    category = serializers.StringRelatedField(many=True)
+    product = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Product
+        fields = ["web_id", "slug", "name", "description", "category", "product"]
+        read_only = True
+        editable = False
+
+
+
