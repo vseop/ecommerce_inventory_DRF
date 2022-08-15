3d2dcb89d1ee2128d59e67b2b4b30fb022415471
vseop <ivpython@mail.ru>
2022-08-15 21:36:53 +0300

    tests api category_multi_child

--- ecommerce/tests/inventory_fixtures.py
+++ ecommerce/tests/inventory_fixtures.py
@@ -19,6 +19,35 @@ def category_with_child(db):
 
 
 @pytest.fixture
+def category_with_multiple_children(db):
+    record = Category.objects.build_tree_nodes(
+        {
+            "id": 1,
+            "name": "parent",
+            "slug": "parent",
+            "children": [
+                {
+                    "id": 2,
+                    "parent_id": 1,
+                    "name": "child",
+                    "slug": "child",
+                    "children": [
+                        {
+                            "id": 3,
+                            "parent_id": 2,
+                            "name": "grandchild",
+                            "slug": "grandchild",
+                        }
+                    ],
+                }
+            ],
+        }
+    )
+    category = Category.objects.bulk_create(record)
+    return category
+
+
+@pytest.fixture
 def single_product(db, category_with_child):
     product = Product.objects.create(
         web_id="123456789",
