0b7e3a74e20922856d2ea83fd070587331314104
vseop <ivpython@mail.ru>
2022-08-18 10:23:12 +0300

    search add views

--- ecommerce/search/views.py
+++ ecommerce/search/views.py
@@ -1,34 +1,34 @@
-from rest_framework.views import APIView
-from rest_framework.pagination import LimitOffsetPagination
 from django.http import HttpResponse
-from elasticsearch_dsl import Q
-
-from ecommerce.drf.serializer import ProductInventorySerializer
+from ecommerce.drf_2.serializer import ProductInventorySearchSerializer
 from ecommerce.search.documents import ProductInventoryDocument
+from elasticsearch_dsl import Q
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.views import APIView
 
 
 class SearchProductInventory(APIView, LimitOffsetPagination):
-    productinvetory_serializer = ProductInventorySerializer
+    productinventory_serializer = ProductInventorySearchSerializer
     search_document = ProductInventoryDocument
 
-    def get(self, request, query):
+    def get(self, request, query=None):
         try:
             q = Q(
-                'multi_match',
+                "multi_match",
                 query=query,
-                fields=[
-                    'product.name'
-                ], fuzziness='auto') & Q(
-                'bool',
+                fields=["product.name", "product.web_id", "brand.name"],
+                fuzziness="auto",
+            ) & Q(
                 should=[
-                    Q('match', is_default=True),
-                ], minimum_should_match=1)
+                    Q("match", is_default=True),
+                ],
+                minimum_should_match=1,
+            )
 
             search = self.search_document.search().query(q)
             response = search.execute()
 
             results = self.paginate_queryset(response, request, view=self)
-            serializer = self.productinvetory_serializer(results, many=True)
+            serializer = self.productinventory_serializer(results, many=True)
             return self.get_paginated_response(serializer.data)
 
         except Exception as e:
