8cca7a5c41300fbf386b6ade252de189498d7a36
vseop <ivpython@mail.ru>
2022-08-21 21:21:51 +0300

    promotion -> new model

--- ecommerce/promotion/models.py
+++ ecommerce/promotion/models.py
@@ -1,7 +1,52 @@
+
+from django.core.exceptions import ValidationError
 from django.db import models
+from ecommerce.inventory.models import ProductInventory
+
 
 class PromoType(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Coupon(models.Model):
+    name = models.CharField(max_length=255)
+    coupon_code = models.CharField(max_length=20)
+
+class Promotion(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    promo_reduction = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=False)
+    is_schedule = models.BooleanField(default=False)
+    promo_start = models.DateField()
+    promo_end = models.DateField()
+
+    products_on_promotion = models.ManyToManyField(
+        ProductInventory,
+        related_name="products_on_promotion",
+        through="ProductsOnPromotion",
+    )
+
+    promo_type = models.ForeignKey(
+        PromoType,
+        related_name="promotype",
+        on_delete=models.PROTECT,
+    )
+
+    coupon = models.ForeignKey(
+        Coupon,
+        related_name="coupon",
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+    )
+
+    def clean(self):
+        if self.promo_start > self.promo_end:
+            raise ValidationError("End data before the start date")
 
     def __str__(self):
         return self.name
