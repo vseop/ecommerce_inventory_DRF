c5a1f4b2513a916b15212cf241b8224d5b8db6d1
vseop <ivpython@mail.ru>
2022-08-22 12:20:22 +0300

    promotion -> new model ProductsOnPromotion

--- ecommerce/promotion/models.py
+++ ecommerce/promotion/models.py
@@ -1,5 +1,7 @@
+from decimal import Decimal
 
 from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 from django.db import models
 from ecommerce.inventory.models import ProductInventory
 
@@ -14,6 +16,7 @@ class PromoType(models.Model):
 class Coupon(models.Model):
     name = models.CharField(max_length=255)
     coupon_code = models.CharField(max_length=20)
+
 
 class Promotion(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -50,3 +53,30 @@ class Promotion(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProductsOnPromotion(models.Model):
+    product_inventory_id = models.ForeignKey(
+        ProductInventory,
+        related_name="ProductInventoryOnPromotion",
+        on_delete=models.PROTECT,
+    )
+    promotion_id = models.ForeignKey(
+        Promotion,
+        related_name="promotion",
+        on_delete=models.CASCADE,
+    )
+    promo_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=Decimal("0.00"),
+        validators=[
+            MinValueValidator(Decimal("0.00")),
+        ],
+    )
+    price_override = models.BooleanField(
+        default=False,
+    )
+
+    class Meta:
+        unique_together = (("product_inventory_id", "promotion_id"),)
